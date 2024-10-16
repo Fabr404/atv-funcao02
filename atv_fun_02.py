79f17733753ea0fffe9bf6d6e1df24422ed9e2f9
fabr404 <fabriciofelixsilva11@gmail.com>
2024-10-16 13:57:19 -0300

    atv função 02

---
+++ atv_fun_02.py
@@ -0,0 +1,12 @@
+# Crie uma função chamada verificar_paridade que receba um número inteiro como argumento e retorne uma mensagem indicando se o número é par ou ímpar.
+
+# reseber o valor
+
+def fun():
+    if numero % 2==0:
+        return(" e par ")
+    else:
+        return(" e impar ")
+number = int(input(" seu numero : "))
+verificar_a_paridade = verificarParidade(number)
+print(f"o numero é {verificar_a_paridade}")  
